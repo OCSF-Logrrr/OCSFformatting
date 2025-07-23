cc2b0222035faed647f4fcfe2b09801ab95681f5
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2025-07-24 01:20:49 +0900

    Update mapping.py

--- OCSF_Normalization/app/mapping.py
+++ OCSF_Normalization/app/mapping.py
@@ -4,10 +4,21 @@ from app.llm import call_llm
 from app.schema_loader import load_class_json
 import re
 import json
+import time
 import asyncio
 
 async def normalize_log(raw_log, class_uid):
     schema = load_class_json(class_uid)
+
+    pattern = r'^(\d+)\s+(\d+)\s+(eni-[a-f0-9]+|-)\s+([0-9.]+|-)\s+([0-9.]+|-)\s+(\d+|-)\s+(\d+|-)\s+(\d+|-)\s+(\d+|-)\s+(\d+|-)\s+(\d+|-)\s+(\d+|-)\s+(ACCEPT|REJECT|-)\s+(OK|NODATA|SKIPDATA|-)$'
+    fields = ['version', 'account_id', 'interface_id', 'srcaddr', 'dstaddr', 'srcport', 'dstport', 'protocol', 'packets', 'bytes', 'start', 'end', 'action', 'log_status']
+
+    for log_entry in raw_log:
+        if log_entry == "message":
+            match = re.match(pattern, log_entry.strip())
+            if match:
+                log_entry['message'] = dict(zip(fields, match.groups()))
+
 
     prompt = f"""
 You are given a raw security log and a JSON schema that follows the OCSF (Open Cybersecurity Schema Framework).
@@ -30,6 +41,7 @@ OCSF JSON schema:
     try:
         loop = asyncio.get_running_loop()
         response = await loop.run_in_executor(None, call_llm, prompt)
+        time.sleep(0.8) # consider
         return json.loads(response)
 
     except json.JSONDecodeError:
