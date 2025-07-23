8072c6237358c19e045a98322f3a3a2936aaebb2
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2025-07-24 01:27:28 +0900

    Update kafka_config.py

--- OCSF_Normalization/configs/kafka_config.py
+++ OCSF_Normalization/configs/kafka_config.py
@@ -2,7 +2,7 @@
 
 # 원본 로그를 수신할 Kafka 클러스터 설정
 INPUT_KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
-INPUT_KAFKA_TOPIC = "raw-logs"
+INPUT_KAFKA_TOPIC = ["raw-logs"]
 INPUT_GROUP_ID = "ocsf-mapper-group"
 
 # 매핑된 로그를 전송할 Kafka 클러스터 설정
